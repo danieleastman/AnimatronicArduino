268d58c14149373f458fa829638fd6ca45033d24
Ryan Brideau <ryan.brideau@gmail.com>
2015-07-04 20:44:32 -0300

    Did first pass at smoothing

--- AnimatronicArduino/AnimatronicArduino.ino
+++ AnimatronicArduino/AnimatronicArduino.ino
@@ -3,25 +3,28 @@
 #include "utility/Adafruit_PWMServoDriver.h"
 #include <AccelStepper.h>
 
-// Used for the sensor that baselines the motors
+// Initialize variables for the sensor that baselines the motors
 const int sensorPin = 0;
 int sensorLevel = 0;
-Adafruit_MotorShield motorShieldA(0x60);
 
 // Set up motors
+Adafruit_MotorShield motorShieldA(0x60);
 
-// Position of Motor A relative to Sensor A
+// Position of Motor A relative to Sensor A in centimetres
 #define motorA2x 30
 #define motorA2y 0
 
 Adafruit_StepperMotor *motorA2 = motorShieldA.getStepper(200, 2);
-int motorA2Angle, motorA2Steps;
+int motorA2Steps; // Used to control position of motor
+
+// Set up functions that guide how the motor moves forward and backward
 void forwardstep2() {  
   motorA2->onestep(FORWARD, DOUBLE);
 }
 void backwardstep2() {  
   motorA2->onestep(BACKWARD, DOUBLE);
 }
+// Add acceleration to the stepper
 AccelStepper motorA2acc(forwardstep2, backwardstep2);
 
 // Set up the range finders
@@ -36,6 +39,15 @@ float xCoord, yCoord;
 float lastAngle = 90; // Used to prevent erratic readings
 double angle;
 
+
+// Set up variables for smoothing the stepper motion
+const int numReadings  = 3; // Number of readings to average
+int readingIndex = 0; // Index of current reading
+float readings[numReadings]; // Array for holding readings
+float readingsTotal = 0.0; // Running total
+float smoothedAngle = 0.0;
+
+
 void setup() {
   Serial.begin(9600);
   motorShieldA.begin();
@@ -43,6 +55,11 @@ void setup() {
   // Set up the pins for the ultrasonic sensors
   pinMode(pwPinA, INPUT);
   pinMode(pwPinB, INPUT);
+
+  // Set up the smoothing array by making it all 0s
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
 
   motorA2acc.setSpeed(50);
   sensorLevel = analogRead(sensorPin);
@@ -58,6 +75,7 @@ void setup() {
   motorA2acc.setAcceleration(600.0);
 }
 
+// Calculates the angle of the tracked object relative to the first sensor
 float angleCalc(float distA, float distB) {
   float angleRad;
   // Law of Cosines rearranged
@@ -72,6 +90,7 @@ float angleCalc(float distA, float distB) {
   }
 }
 
+// Calculates where to position the motor to point at the object.
 int motorPosition(float xCoord, float yCoord) {
   float rad, deg;
   int steps;
@@ -85,12 +104,12 @@ int motorPosition(float xCoord, float yCoord) {
   return steps;
 }
 
-int cycleCount = 16000;
+int cycleCount = 8000;
 
 void loop() {  
   // Serial.print(", Motor A Steps: ");
-  // Serial.println(motorA2Angle);
-  if (cycleCount == 16000){
+  // Serial.println(motorA2Steps);
+  if (cycleCount == 8000){
     // Get the distance from sensor A
     pulseA = pulseIn(pwPinA, HIGH);
     pulseA = pulseA / 10.0; // 10 uS per cm
@@ -105,19 +124,34 @@ void loop() {
     angle = angleCalc(pulseA, pulseB);
     // Serial.print(", Angle: ");
     //  Serial.print(angle);
+
+    // Smooth the movement
+    readingsTotal = readingsTotal - readings[readingIndex];
+    readings[readingIndex] = angle;
+    readingsTotal = readingsTotal + readings[readingIndex];
+    readingIndex = readingIndex + 1;
+    // Loop around the array
+    if (readingIndex >= numReadings) {
+      readingIndex = 0;
+    }
+
+    smoothedAngle = readingsTotal / (float)numReadings;
     
-    xCoord = pulseA*cos(angle);
+    xCoord = pulseA*cos(smoothedAngle);
     // All y values are negative because of the coordinate system used relative to the position of the sensors.
-    yCoord = -1*pulseA*sin(angle);
+    yCoord = -1*pulseA*sin(smoothedAngle);
     // Serial.print("X: ");
     // Serial.print(xCoord); 
     // Serial.print(", Y: ");
     // Serial.print(yCoord);
     // Serial.print(", Sensor A Angle: ");
     // Serial.print(angle);
+    // Serial.print(", Smoothed: ");
+    // Serial.println(smoothedAngle);
     
-    motorA2Angle = motorPosition(xCoord, yCoord);
-    motorA2acc.moveTo(motorA2Angle);
+    motorA2Steps = motorPosition(xCoord, yCoord);
+
+    motorA2acc.moveTo(motorA2Steps);
     cycleCount = 0;
   } else {
     cycleCount = cycleCount + 1;
