caa82375170117ffa3c544301b61fbfdec2a4bd4
Ryan Brideau <ryan.brideau@gmail.com>
2015-10-18 01:04:21 -0300

    Added tracking

---
+++ Master/Master.ino
@@ -0,0 +1,156 @@
+// Serial port setup
+
+#include <SoftEasyTransfer.h>
+#include <SoftwareSerial.h>
+SoftwareSerial XBee =  SoftwareSerial(2, 3);
+// SoftEasyTransfer ETin, ETout; 
+SoftEasyTransfer ETout; 
+
+// Set of variables for choosing which file to play
+//int currentFileNumber = 99;
+//int fileNumber = 0;
+//bool changed = true;
+
+// Distance between the range finders
+#define distRF 148.0
+#define rangeA 7 // RF A
+#define rangeB 8 // RF B
+long pulseA, pulseB;
+
+// Set up variables needed to smooth readings
+float lastAngle = 90; // Used to prevent erratic readings
+double angle;
+// Set up variables for smoothing the stepper motion
+const int numReadings  = 3; // Number of readings to average
+int readingIndex = 0; // Index of current reading
+float readings[numReadings]; // Array for holding readings
+float readingsTotal = 0.0; // Running total
+float smoothedAngle = 0.0;
+
+// Set up variables for triangulation
+float xCoord, yCoord;
+
+#define NUM_STATUE 1
+int statues[] = {1};
+
+struct SEND_DATA_STRUCTURE{
+  int unitId;
+  int commandType;
+  int xPos;
+  int yPos;
+  // int audio;
+};
+SEND_DATA_STRUCTURE sendData;
+
+// struct RECEIVE_DATA_STRUCTURE{
+//   int blink;
+// };
+// RECEIVE_DATA_STRUCTURE receiveData;
+
+void setup() {  
+  Serial.begin(9600);
+  Serial.println("Testing Up!");
+
+  // Set up the pins for the ultrasonic sensors
+  pinMode(rangeA, INPUT);
+  pinMode(rangeB, INPUT);
+  // set the data rate for the SoftwareSerial port
+  XBee.begin(9600);
+
+  ETout.begin(details(sendData), &XBee);
+  // ETin.begin(details(receiveData), &XBee);
+
+  // Set up the smoothing array by making it all 0s
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+    }
+  }
+
+// Calculates the angle of the tracked object relative to the first sensor
+float angleCalc(float distA, float distB) {
+  float angleRad;
+  // Law of Cosines rearranged
+  angleRad = acos((distA * distA + distRF * distRF - distB * distB) / (2 * distA * distRF));
+
+  // Prevent erratic readings
+  if (isnan(angleRad) == 1) {
+    return lastAngle;
+  } else {
+    lastAngle = angleRad;
+    return angleRad;
+  }
+}
+
+int cycleCount = 16000;
+
+void loop() {
+  if (cycleCount == 16000) {
+    // Get the distance from sensor A and B
+    pulseA = pulseIn(rangeA, HIGH);
+    pulseA = pulseA / 10.0; // 10 uS per cm
+    pulseB = pulseIn(rangeB, HIGH);
+    pulseB = pulseB / 10.0;
+
+    // Get the angle relative to the first sensor
+    angle = angleCalc(pulseA, pulseB);
+
+    // Smooth the movement
+    readingsTotal = readingsTotal - readings[readingIndex]; // Subtract the old
+    readings[readingIndex] = angle; // Insert the new
+    readingsTotal = readingsTotal + readings[readingIndex]; // Add the new
+    readingIndex = readingIndex + 1; // Move to the next index
+    // Loop around the array
+    if (readingIndex >= numReadings) {
+      readingIndex = 0; // Start the index back at 0
+    }
+    // Take the average of the last three readings
+    smoothedAngle = readingsTotal / (float)numReadings;
+    // Find the x-coordinate and y-coordinate of the person. All y values are
+    // negative because of the coordinate system used relative to the position
+    // of the sensors.
+    xCoord = pulseA * cos(smoothedAngle);
+    yCoord = -1 * pulseA * sin(smoothedAngle);
+
+    for(int i = 0; i < NUM_STATUE; i++) {
+      sendData.unitId = statues[i];
+      sendData.commandType = 1;
+      sendData.xPos = xCoord;
+      Serial.print(sendData.xPos);
+      sendData.yPos = yCoord;
+      Serial.print(", ");
+      Serial.println(sendData.yPos);
+      ETout.sendData();
+      // Serial.println("---------------");
+      // Serial.print("Statue: ");
+      // Serial.println(statues[i]);
+      // Serial.print("Command: ");
+      // Serial.println("MoveTo");
+      // Serial.print("X: ");
+      // Serial.println(xCoord);
+      // Serial.print(", Y: ");
+      // Serial.println(yCoord);
+      // Serial.println("---------------");
+    }
+//
+//    if (ETin.receiveData()) {
+//        digitalWrite(ledPin, HIGH);
+//        delay(50);
+//        digitalWrite(ledPin, LOW);
+//    }
+
+    cycleCount = 0;
+  }
+
+  // Generate the file number to play from the angle of the pot
+  // fileNumber = constrain(map(angle, 0, 180, 1, 12), 1, 11);
+  // if (currentFileNumber != fileNumber) {
+  //   changed = true;
+  //   currentFileNumber = fileNumber;
+  // } else {
+  //   changed = false;
+  // }
+
+  cycleCount = cycleCount + 1;
+
+}
+
--- Slave/Slave/Slave.ino
+++ Slave/Slave/Slave.ino
@@ -1,95 +1,132 @@
 #include <EasyTransfer.h>
-#include <SoftwareSerial.h>
+// #include <SoftwareSerial.h>
 #include <Servo.h>
 #include <SPI.h>
-#include <Adafruit_VS1053.h>
-#include <SD.h>
+// #include <Adafruit_VS1053.h>
+// #include <SD.h>
 
 // The unique ID of the unit
-int statueId = 3;
+int statueId = 1;
+// Statue position
+#define statueX 95.0
+#define statueY 10.0
 
-Servo myServo;
+int statueCurrAngle = 0;
+int statueAngle = 0;
+
+Servo statueServo;
+
+// SoftwareSerial XBee =  SoftwareSerial(2, 3);
 
 // Set up the audio card
-#define SHIELD_RESET  -1      // VS1053 reset pin (unused!)
-#define SHIELD_CS     7      // VS1053 chip select pin (output)
-#define SHIELD_DCS    6      // VS1053 Data/command select pin (output)
+// #define SHIELD_RESET  -1      // VS1053 reset pin (unused!)
+// #define SHIELD_CS     7      // VS1053 chip select pin (output)
+// #define SHIELD_DCS    6      // VS1053 Data/command select pin (output)
 
 // These are common pins between breakout and shield
-#define CARDCS 4     // Card chip select pin
+// #define CARDCS 4     // Card chip select pin
 // DREQ should be an Int pin, see http://arduino.cc/en/Reference/attachInterrupt
-#define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
+// #define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
 
-Adafruit_VS1053_FilePlayer musicPlayer = 
-  // create breakout-example object!
-  //Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
-  // create shield-example object!
-  Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
-
-String filename;
-int fileNumber;
-char filename_char[10];
+// Adafruit_VS1053_FilePlayer musicPlayer = 
+//   // create breakout-example object!
+//   //Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
+//   // create shield-example object!
+//   Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
 
 // Set up easy transfer
-EasyTransfer ETin, ETout;
+// EasyTransfer ETin, ETout;
+EasyTransfer ETin;
 struct RECEIVE_DATA_STRUCTURE{
   int unitId;
-  int commandId;
-  int angle;
-  int audio;
+  int commandType;  
+  int xPos;
+  int yPos;
+  // int audio;
 };
 RECEIVE_DATA_STRUCTURE receiveData;
 
-struct SEND_DATA_STRUCTURE{
-  int blink;
-};
-SEND_DATA_STRUCTURE sendData;
+// struct SEND_DATA_STRUCTURE{
+//   int blink;
+// };
+// SEND_DATA_STRUCTURE sendData;
 
 void setup() {
   Serial.begin(9600);
+  // XBee.begin(9600);
   ETin.begin(details(receiveData), &Serial);
-  ETout.begin(details(sendData), &Serial);
+  // ETout.begin(details(sendData), &Serial);
 
-  myServo.attach(2);
+  statueServo.attach(9);
 
-  if (! musicPlayer.begin()) { // initialise the music player
-     // Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
-     while (1);
-  }
+  // if (! musicPlayer.begin()) { // initialise the music player
+  //    // Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+  //    while (1);
+  // }
   
-  SD.begin(CARDCS);    // initialise the SD card
+  // SD.begin(CARDCS);    // initialise the SD card
 
   // If DREQ is on an interrupt pin (on uno, #2 or #3) we can do background
   // audio playing
-  musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
+  // musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
 
-  musicPlayer.playFullFile("Left.mp3");
+  // musicPlayer.playFullFile("Left.mp3");
 }
+
+
+// Calculates where to position the motor to point at the object detected.
+float motorPositionDeg(float xCoord, float yCoord, float motorX, float motorY) {
+  float rad, deg;
+  // atan 2 takes the arctan and finds which quadrant it is in
+  // based on the x and y values provided
+  rad = atan2((yCoord - motorY), (xCoord - motorX));
+  // Return 'geometric' angle in degrees
+  deg = rad * -180 / M_PI;
+  // Because servos measure angle backwards for some reason, we have to
+  // map the geometric angle to a servo angle
+  deg = map(deg, 0, 180, 180, 0);
+  // Serial.println(deg);
+  return deg;
+}
+
 
 // the loop function runs over and over again forever
 void loop() {
   if (ETin.receiveData()) {
-
-    // musicPlayer.stopPlaying();
-    myServo.write(receiveData.angle);
-    sendData.blink = 1;
-    ETout.sendData();
-
-    fileNumber = receiveData.audio;
-
-    if (fileNumber < 10) {
-      filename = "0";
-      filename.concat(fileNumber);
-      filename.concat(".mp3");
-    } else {
-      filename = "";
-      filename.concat(fileNumber);
-      filename.concat(".mp3");
+//  Serial.println("---------------");
+//  Serial.print("Statue: ");
+//  Serial.println(receiveData.unitId);
+//  Serial.print("Command: ");
+//  Serial.println(receiveData.commandType);
+//  Serial.print("X: ");
+//  Serial.print(receiveData.xPos);
+//  Serial.print(", Y: ");
+//  Serial.println(receiveData.yPos);
+  // Convert the x,y coordinate to an angle
+  statueAngle = motorPositionDeg(receiveData.xPos, receiveData.yPos, statueX, statueY);
+  if (abs(statueCurrAngle - statueAngle) > 2) {
+      statueServo.write(statueAngle);
+      statueCurrAngle = statueAngle;
     }
 
-    // Serial.println(filename);
-    filename.toCharArray(filename_char, 10);
-    // Serial.println(filename_char);
-    //musicPlayer.startPlayingFile(filename_char);
+  // sendData.blink = 1;
+  // ETout.sendData();
+
+  // fileNumber = receiveData.audio;
+
+  // if (fileNumber < 10) {
+  //   filename = "0";
+  //   filename.concat(fileNumber);
+  //   filename.concat(".mp3");
+  // } else {
+  //   filename = "";
+  //   filename.concat(fileNumber);
+  //   filename.concat(".mp3");
+  // }
+
+  // Serial.println(filename);
+  // filename.toCharArray(filename_char, 10);
+  // // Serial.println(filename_char);
+  // musicPlayer.startPlayingFile(filename_char);
   }
 }
