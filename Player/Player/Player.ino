deec977310594faa6ccc3640ec84cd37da27fbfe
Brendan Wood <brendan.wood@brendanwood-ltm.internal.salesforce.com>
2015-10-21 20:55:12 -0300

    Hashed out some script parsing.

--- Player/Player/Player.ino
+++ Player/Player/Player.ino
@@ -1,6 +1,6 @@
+
 #include <SPI.h>
 #include <SD.h>
-#include <Adafruit_VS1053.h>
 
 #define SHIELD_RESET  -1      // VS1053 reset pin (unused!)
 #define SHIELD_CS     7      // VS1053 chip select pin (output)
@@ -8,67 +8,121 @@
 
 // These are common pins between breakout and shield
 #define CARDCS 4     // Card chip select pin
-// DREQ should be an Int pin, see http://arduino.cc/en/Reference/attachInterrupt
-#define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
 
-Adafruit_VS1053_FilePlayer musicPlayer = 
-  // create breakout-example object!
-  //Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
-  // create shield-example object!
-  Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
+#define MAX_LINE_LENGTH 128
+#define MAX_ARG_LENGTH 32
 
-//void unpack_message(char messageBuffer[], int *unitIdReceived, int *commandId, int *angle, char audio[]) {
-//  sscanf(messageBuffer, "%d:%d:%d:%s", unitIdReceived, commandId, angle, audio);
-//}
 
-// the setup function runs once when you press reset or power the board
-void setup() {
-  Serial.begin(9600);
-  Serial.println("Testing motors and audio begins!");
+
+void parse_line_args(char line[], char statue_id[], char command[], char command_args[],  char pause[]) {
+  int i = 0;
+  int arg_index = 0;
+  int arg_id = 0;  
   
-  if (! musicPlayer.begin()) { // initialise the music player
-     Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
-     while (1);
+  char c = line[i];
+  while (c != 0x0) {
+    if (c == 0x3A) {
+      // arg delimiter (colon)
+      
+      
+
+      // terminate arg with null
+      if (arg_id == 0) {
+        // statue_id
+        statue_id[arg_index] = 0x0;
+      }
+      else if (arg_id == 1) {
+        // command
+        command[arg_index] = 0x0;
+      }
+      else if (arg_id == 2) {
+        // command args
+        command_args[arg_index] = 0x0;
+      }
+      else if (arg_id == 3) {
+        // pause before next command
+        pause[arg_index] = 0x0;
+      }
+
+      arg_index = 0;
+      arg_id++;
+    }
+    else {
+
+      // save character into appropriate arg
+      if (arg_id == 0) {
+        // statue_id
+        statue_id[arg_index] = c;
+      }
+      else if (arg_id == 1) {
+        // command
+        command[arg_index] = c;
+      }
+      else if (arg_id == 2) {
+        // command args
+        command_args[arg_index] = c;
+      }
+      else if (arg_id == 3) {
+        // pause before next command
+        pause[arg_index] = c;
+      }
+
+      arg_index++;
+    }
+    
+    i++;
+    c = line[i];
   }
-  Serial.println(F("VS1053 found"));
-  
-  SD.begin(CARDCS);    // initialise the SD card
-  
-  // Set volume for left, right channels. lower numbers == louder volume!
-  //musicPlayer.setVolume(20,20);
-
-  // Timer interrupts are not suggested, better to use DREQ interrupt!
-  //musicPlayer.useInterrupt(VS1053_FILEPLAYER_TIMER0_INT); // timer int
-
-  // If DREQ is on an interrupt pin (on uno, #2 or #3) we can do background
-  // audio playing
-  musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
-
-  musicPlayer.playFullFile("Left.mp3");
-  //musicPlayer.playFullFile("01.mp3");
 }
 
-// the loop function runs over and over again forever
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Initializing SD card...");
+  SD.begin(CARDCS);    // initialise the SD card
+  Serial.println("SD card initialized.");
+
+  File scriptFile = SD.open("script.txt");
+
+  if (scriptFile && scriptFile.available()) {
+    char line[MAX_LINE_LENGTH];
+    char statue_id[MAX_ARG_LENGTH];
+    char command[MAX_ARG_LENGTH];
+    char command_args[MAX_ARG_LENGTH];
+    char pause[MAX_ARG_LENGTH];
+    
+    int i = 0;
+    char c = scriptFile.read();
+    while (c != EOF) {
+
+      if (c == 0xA) {
+        // end of line, parse and save
+        line[i] = 0x0;
+        i = 0;
+        Serial.println(line);
+
+        parse_line_args(line, statue_id, command, command_args, pause);
+        Serial.print("  "); Serial.println(statue_id);
+        Serial.print("  "); Serial.println(command);
+        Serial.print("  "); Serial.println(command_args);
+        Serial.print("  "); Serial.println(pause);
+        Serial.println();
+      }
+      else {
+        line[i] = (char) c;
+        i++;
+      }
+      
+      c = scriptFile.read();
+      
+    }
+    scriptFile.close();
+  }
+  else {
+    Serial.println("error opening script.txt");
+  }
+}
+
+
 void loop() {
-  musicPlayer.playFullFile("Left.mp3");
-  delay(2000);
-//  
-//  if (Serial.available()) {
-//
-//    receivedMessage = Serial.readStringUntil('@');
-//    Serial.println(receivedMessage);
-//    receivedMessage.toCharArray(receiveMessageBuffer, MESSAGE_BUFFER_SIZE);
-//    unpack_message(receiveMessageBuffer, &receiveUnitId, &receiveCommandId, &receiveAngle, receiveAudio);
-//    
-////    Serial.println(receiveUnitId);
-////    Serial.println(receiveCommandId);
-////    Serial.println(receiveAngle);
-////    Serial.println(receiveAudio);
-////    Serial.println("");
-//
-//    musicPlayer.stopPlaying();
-//    myServo.write(receiveAngle);
-//    
-//    musicPlayer.startPlayingFile(receiveAudio);
-//  }
+
 }
